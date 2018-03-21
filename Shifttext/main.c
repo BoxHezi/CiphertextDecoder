fbc94f44eb38290ffe1037b33eee9a80f18d3e5e
Boxuan Lu <547181293@qq.com>
2018-03-21 21:44:08 +1100

    complete

--- Shifttext/main.c
+++ Shifttext/main.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
 #include <mem.h>
+#include <stdlib.h>
 
 typedef enum boolean {
     FALSE = 0,
     TRUE
 } Boolean;
 
-int main(int argc, char ** args) {
-    char data[20];
+void readRestOfLine();
 
-    printf("Please enter cipher-text you want to decode: ");
-    fgets(data, sizeof(data), stdin);
-    printf("%s\n", data);
-    /*printf("%d\n", argc);
+int main() {
+    char data[20 + 2];
+    char tempChar;
+    int len = 0;
+    Boolean validInput = FALSE;
+    FILE *fp;
+    fp = fopen("painetext.txt", "r+");
 
-    for(int i = 0; i < argc; i++) {
-        printf("%s", args[i]);
-    }*/
+    do {
+        printf("Please enter cipher-text you want to decode: ");
+        fgets(data, sizeof(data), stdin);
+        if (data[strlen(data) - 1] != '\n') {
+            printf("Too long, try again!\n");
+            readRestOfLine();
+        } else {
+            data[strlen(data) - 1] = '\0';
+            validInput = TRUE;
+        }
+    } while (!validInput);
 
-    printf("Hello, World!\n");
-    return 0;
+    len = strlen(data);
+
+    for (int i = 0; i < 26; i++) {
+        printf("Shift by %d: ", i);
+        fprintf(fp, "Shift by %d: ", i);
+        for (int j = 0; j < len; j++) {
+            tempChar = (char) (data[j] + i);
+            if (tempChar > 'Z') {
+                tempChar = (char) (tempChar - 26);
+            }
+            printf("%c", tempChar);
+            fprintf(fp, "%c", tempChar);
+        }
+        printf("\n");
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
+
+void readRestOfLine() {
+    int ch;
+    while (ch = getc(stdin), ch != EOF && ch != '\n') {}
+
+    clearerr(stdin);
 }
